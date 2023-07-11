c8eb824dc3c5bdfc18bc696a6d2609ff5384098f
Keith-Ocitti <keithdimaria@gmail.com>
2023-07-11 12:20:40 +0300

    first-commit

--- secure-health/src/app/layout.js
+++ secure-health/src/app/layout.js
@@ -1,3 +1,11 @@
+// import Font Awesome CSS
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+// Tell Font Awesome to skip adding the CSS automatically
+// since it's already imported above
+config.autoAddCss = false;
 export const metadata = {
   title: "Secure Health",
 };
